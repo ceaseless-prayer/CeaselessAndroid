c37f04bb904dabd1784acadafa4c9bcf5d0f9718
Ben Johnson <bwj.johnson@gmail.com>
2015-10-03 18:04:45 -0700

    Implemented the PeopleManagerInterface

--- app/src/main/java/org/theotech/ceaselessandroid/person/PersonManagerImpl.java
+++ app/src/main/java/org/theotech/ceaselessandroid/person/PersonManagerImpl.java
@@ -1,5 +1,7 @@
 package org.theotech.ceaselessandroid.person;
 
+import java.util.List;
+
 import io.realm.Realm;
 
 /**
@@ -9,5 +11,33 @@ public class PersonManagerImpl implements PersonManager {
 
     private Realm realm;
 
+    @Override
+    public List<Person> getNextPeopleToPrayFor(int n) {
+        return null;
+    }
 
+    @Override
+    public List<Person> getAllPeople() {
+        return null;
+    }
+
+    @Override
+    public Person getPerson(String id) {
+        return null;
+    }
+
+    @Override
+    public boolean removePerson(String id) {
+        return false;
+    }
+
+    @Override
+    public boolean favoritePerson() {
+        return false;
+    }
+
+    @Override
+    public boolean unfavoritePerson() {
+        return false;
+    }
 }
