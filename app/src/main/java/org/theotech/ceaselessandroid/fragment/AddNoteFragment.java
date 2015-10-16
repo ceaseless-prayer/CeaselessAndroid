6bbc0f55fcfb3a33967bfe041c4ef92870ca9e22
UberEcks <anirudhroxrulz@gmail.com>
2015-10-16 07:09:29 -0700

    Hiding keyboard when save is pressed

--- app/src/main/java/org/theotech/ceaselessandroid/fragment/AddNoteFragment.java
+++ app/src/main/java/org/theotech/ceaselessandroid/fragment/AddNoteFragment.java
@@ -2,10 +2,12 @@ package org.theotech.ceaselessandroid.fragment;
 
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -80,6 +82,13 @@ public class AddNoteFragment extends Fragment {
         saveNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // hide keyboard if it's open
+                View view = getActivity().getCurrentFocus();
+                if (view != null) {
+                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
+                // add the note to all the tagged people
                 for (PersonPOJO taggedPerson : taggedPeople) {
                     personManager.addNote(taggedPerson.getId(), null, noteText.getText().toString());
                 }
