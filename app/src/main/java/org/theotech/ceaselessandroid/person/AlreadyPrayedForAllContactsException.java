5f0bcef84912fa153d864407b482638742d0bb62
Ceasing One <ceasingone@gmail.com>
2015-11-10 21:24:57 -0800

    Extend Exception in .*Exception.java

--- app/src/main/java/org/theotech/ceaselessandroid/person/AlreadyPrayedForAllContactsException.java
+++ app/src/main/java/org/theotech/ceaselessandroid/person/AlreadyPrayedForAllContactsException.java
@@ -3,11 +3,12 @@ package org.theotech.ceaselessandroid.person;
 /**
  * Created by Ben Johnson on 10/3/15.
  */
-public class AlreadyPrayedForAllContactsException extends Throwable {
+public class AlreadyPrayedForAllContactsException extends Exception {
 
     long numPeople;
 
     public AlreadyPrayedForAllContactsException(long numPeople) {
+        super("Already prayed for " + numPeople + " contacts!");
         this.numPeople = numPeople;
     }
 
