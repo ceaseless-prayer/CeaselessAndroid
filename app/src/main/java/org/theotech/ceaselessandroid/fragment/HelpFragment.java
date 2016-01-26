91f9787810eba494dc5a76dff722826e9f2db4d5
travis <travis.kopp@gmail.com>
2016-01-26 16:51:59 -0700

    resolved lint and pmd problems

--- app/src/main/java/org/theotech/ceaselessandroid/fragment/HelpFragment.java
+++ app/src/main/java/org/theotech/ceaselessandroid/fragment/HelpFragment.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
-import android.widget.EditText;
 
 import com.google.android.gms.analytics.Tracker;
 
