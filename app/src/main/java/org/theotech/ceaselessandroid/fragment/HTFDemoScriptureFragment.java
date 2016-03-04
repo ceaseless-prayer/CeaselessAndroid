149ea974ed90271491c17a4be620d9511a470c12
travis <travis.kopp@gmail.com>
2016-03-04 12:55:35 -0700

    added example drawable over verse card

--- app/src/main/java/org/theotech/ceaselessandroid/fragment/HTFDemoScriptureFragment.java
+++ app/src/main/java/org/theotech/ceaselessandroid/fragment/HTFDemoScriptureFragment.java
@@ -1,7 +1,5 @@
 package org.theotech.ceaselessandroid.fragment;
 
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -9,7 +7,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewOverlay;
 import android.widget.TextView;
 
 
