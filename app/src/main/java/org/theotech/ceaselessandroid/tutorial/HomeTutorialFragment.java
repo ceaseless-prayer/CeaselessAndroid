1530dc40132f6fc4ce6178a0beacba5ac33d9d92
chrislim <kirisu@gmail.com>
2016-03-26 10:23:09 -0700

    Refactoring HomeTutorialFragment to solve FindBugs issue.

--- app/src/main/java/org/theotech/ceaselessandroid/tutorial/HomeTutorialFragment.java
+++ app/src/main/java/org/theotech/ceaselessandroid/tutorial/HomeTutorialFragment.java
@@ -1,27 +1,28 @@
 package org.theotech.ceaselessandroid.tutorial;
 
-import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.viewpagerindicator.CirclePageIndicator;
 
 import org.theotech.ceaselessandroid.R;
 import org.theotech.ceaselessandroid.activity.MainActivity;
+import org.theotech.ceaselessandroid.fragment.BlankSupportFragment;
 import org.theotech.ceaselessandroid.fragment.FragmentState;
 import org.theotech.ceaselessandroid.fragment.FragmentStateListener;
 import org.theotech.ceaselessandroid.transformer.ZoomOutPageTransformer;
+
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -33,7 +34,6 @@ import butterknife.ButterKnife;
 public class HomeTutorialFragment extends Fragment {
     private static final String TAG = HomeTutorialFragment.class.getSimpleName();
     private static final Integer CARD_COUNT = 3;
-
     @Bind(R.id.tutorial_viewpager)
     ViewPager viewPager;
     @Bind(R.id.tutorial_indicator)
@@ -71,42 +71,47 @@ public class HomeTutorialFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home_tutorial, container, false);
         ButterKnife.bind(this, view);
 
-        HomeTutorialFragment.HTFFragmentStatePagerAdapter pagerAdapter =
-                new HomeTutorialFragment.HTFFragmentStatePagerAdapter(
-                        ((AppCompatActivity) getActivity()).getSupportFragmentManager());
+       final FragmentStatePagerAdapter pagerAdapter =
+               new FragmentStatePagerAdapter(((AppCompatActivity) getActivity()).getSupportFragmentManager()) {
+                   List<android.support.v4.app.Fragment> pages = Arrays.asList(new android.support.v4.app.Fragment[CARD_COUNT]);
+
+                   @Override
+                   public android.support.v4.app.Fragment getItem(int i) {
+                       if (pages.get(i) == null) {
+                           switch (i) {
+                               case 0:
+                                   pages.set(i, new HTFDemoScriptureFragment());
+                                   break;
+                               case 1:
+                                   pages.set(i, HTFDemoPersonFragment.newInstance(getString(R.string.tutorial_person_name), true));
+                                   break;
+                               case 2:
+                                   pages.set(i, new HTFDemoProgressFragment());
+                                   break;
+                               default:
+                                   pages.set(i, new BlankSupportFragment());
+                                   break;
+                           }
+                       }
+                       return pages.get(i);
+                   }
+
+                   @Override
+                   public int getCount() {
+                       return CARD_COUNT;
+                   }
+               };
+
         viewPager.setAdapter(pagerAdapter);
         viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
         viewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int i) {
-                ((HTFDemoFragment) ((HTFFragmentStatePagerAdapter) viewPager.getAdapter()).getItem(i)).onSelected();
+                ((HTFDemoFragment) pagerAdapter.getItem(i)).onSelected();
             }
         });
         indicator.setViewPager(viewPager);
         return view;
-    }
-
-    class HTFFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
-
-        private android.support.v4.app.Fragment[] fragArray;
-        public HTFFragmentStatePagerAdapter(FragmentManager fm) {
-            super(fm);
-            fragArray = new android.support.v4.app.Fragment[4];
-            fragArray[0] = new HTFDemoScriptureFragment();
-            fragArray[1] = HTFDemoPersonFragment.newInstance(getString(R.string.tutorial_person_name), true);
-            fragArray[2] = new HTFDemoProgressFragment();
-        }
-
-        @Override
-        public android.support.v4.app.Fragment getItem(int i) {
-            return fragArray[i];
-        }
-
-        @Override
-        public int getCount() {
-            return CARD_COUNT;
-        }
-
     }
 
     @Override
