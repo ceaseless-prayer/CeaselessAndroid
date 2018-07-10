af34cbad9f39b43b5eaa0ac5a5e9db9ce6180b98
Chris <kirisu@gmail.com>
2018-07-10 09:49:30 -0700

    fixes pmd error

--- app/src/main/java/org/theotech/ceaselessandroid/fragment/HomeFragment.java
+++ app/src/main/java/org/theotech/ceaselessandroid/fragment/HomeFragment.java
@@ -10,7 +10,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
