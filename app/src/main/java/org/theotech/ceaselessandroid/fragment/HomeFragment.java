ca3c1113624a0b6649165d411c0a5021c2d8cbf0
Chris <kirisu@gmail.com>
2019-06-21 13:56:49 -0700

    Fixing pmd issue

--- app/src/main/java/org/theotech/ceaselessandroid/fragment/HomeFragment.java
+++ app/src/main/java/org/theotech/ceaselessandroid/fragment/HomeFragment.java
@@ -18,7 +18,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.SearchView;
 
 import com.squareup.picasso.Picasso;
